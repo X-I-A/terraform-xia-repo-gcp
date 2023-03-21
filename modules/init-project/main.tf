8662fca7ec7d527ac244d2e79af559c7df71a759
X-I-A <develop@x-i-a.com>
2023-03-21 19:43:19 +0100

    feat: initial release

--- modules/init-project/main.tf
+++ modules/init-project/main.tf
@@ -3,3 +3,9 @@ resource "google_project_service" "cloud_resource_manager_api" {
   service = "cloudresourcemanager.googleapis.com"
   disable_on_destroy = false
 }
+
+resource "google_project_service" "container_registry_api" {
+  project = var.project_id
+  service = "containerregistry.googleapis.com"
+  disable_on_destroy = false
+}
